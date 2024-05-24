55e4bb1b2c2ea6d9a9a73599441d04a8859b1fa7
DeclanLewis <declan.lewis@student.calrossy.nsw.edu.au>
2024-05-24 10:00:08 +1000

    The room file ha been added

---
+++ Room.py
@@ -0,0 +1,54 @@
+import pygame
+
+class Room:
+    def __init__(self, image, list_of_doors, music, speech):
+
+        # Class initialisation
+        self.BG = pygame.image.load(image)
+        self.BG = pygame.transform.smoothscale(self.BG, (1280, 720))
+        self.doors = list_of_doors
+        self.music = music
+        pygame.mixer.init()
+        self.speech = speech
+
+    def draw(self, screen):
+
+        # Draws the room and doors to the screen
+        screen.blit(self.BG, (0, 0))
+        for door in self.doors:
+            door.draw(screen)
+
+    def start_music(self):
+        pygame.mixer.stop()
+        pygame.mixer.music.load(self.music)
+        pygame.mixer.music.play(-1)
+        return True
+
+    def play_speech(self):
+        speech = pygame.mixer.Sound(self.speech)
+        speech.stop()
+        speech.play()
+        print(f"Playing speech {self.speech}")
+        return True
+
+    def stop_speech(self):
+        speech = pygame.mixer.Sound(self.speech)
+        speech.stop()
+        print(f"Current speech {self.speech}")
+
+    def get_next_room(self):
+
+        # a loop to get the correct amount of doors and return what doors
+        for door in self.doors:
+            if door.clicked:
+                door.clicked = False
+                return door.destination
+        return None
+
+
+
+
+
+
+
+
